b1a1fc1a7e54a3be4a52fb0e164cd7995576eabf
museloper <nosf6842@naver.com>
2024-11-17 19:10:24 +0900

    BUILD: 테일윈드 커스텀 컬러 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pastel-red': '#D16D6D',
+        'pastel-yellow': '#D6B86D',
+        'pastel-green': '#6DD6A0',
+        'pastel-blue': '#6D83D6',
+      },
+    },
   },
   plugins: [],
 }
